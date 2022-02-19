c5c0091b92553f5f7292288f8ab98560f5de2348
RaianHossain <raiansanil@gmail.com>
2022-02-19 13:45:59 +0600

    first commit with little bit of ordersscreen

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        return view("components.layouts.orders.index");
+    }
+    
+    public function create()
+    {
+        return view("components.layouts.orders.create");
+    }
+}
