0d50536d162b53e379859a219821ab8d2cd357ca
RaianHossain <raiansanil@gmail.com>
2022-02-22 16:08:57 +0600

    deleted api problem

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return view("components.layouts.products.index");
+        $products = Product::latest()->get();
+        return view("components.layouts.products.index", ['products' => $products]);
     }
     
     public function create()
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/pricelists', [PricelistController::class, 'store'])->name("pricelists.store");
-Route::get('/pricelists', function () {
-    return "Hello";
-});
+
